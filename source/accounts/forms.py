1c60eb630b8cec4fbc47d1d2e9fe937d3922c52d
akyl <akyl.aydarbekov@gmail.com>
2020-03-24 00:25:58 +0600

    Добавил формсет при создании

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -2,12 +2,18 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import inlineformset_factory
+
+from accounts.models import Profile
+
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True, label='Email')
 
     class Meta(UserCreationForm.Meta):
         fields = ('username', 'first_name', 'last_name', 'email')
+
+
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -16,3 +22,12 @@ class SignUpForm(UserCreationForm):
             raise ValidationError('Email has already registered', code='email_registered')
         except User.DoesNotExist:
             return email
+
+class ProfileForm_2(forms.ModelForm):
+    class Meta:
+        model = User
+        exclude = []
+
+
+ProfileFormset = inlineformset_factory(User, Profile, ProfileForm_2, extra=1,
+                                         can_delete=True)
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,14 +1,66 @@
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
-from accounts.forms import SignUpForm
+from accounts.forms import SignUpForm, ProfileFormset
 
 
 class SignUp(CreateView):
     form_class = SignUpForm
     template_name = 'user_create.html'
     success_url = reverse_lazy('accounts:login')
+
+    def get_context_data(self, **kwargs):
+        form_type = self.request.GET.get('form')
+        if 'formset' not in kwargs:
+            kwargs['formset'] = ProfileFormset()
+
+        # if form_type == 'only_file':
+        #     if self.request.POST and self.request.FILES:
+        #         kwargs['form'] = FormForFile(self.request.POST, self.request.FILES)
+        #     else:
+        #         kwargs['form'] = FormForFile()
+        #     self.template_name = 'experts/create_for_file.html'
+        return super().get_context_data(**kwargs)
+    #
+    # def post(self, request, *args, **kwargs):
+    #     # form_type = request.GET.get('form')
+    #     self.object = None
+    #     # if form_type == 'only_file':
+    #     #     self.template_name = 'experts/create_for_file.html'
+    #     #     form = FormForFile(request.POST, request.FILES)
+    #     #     if form.is_valid():
+    #     #         return self.form_valid(form)
+    #     #     return self.form_invalid(form)
+    #     # else:
+    #     form = self.get_form()
+    #
+    #     formset = ProfileFormset(data=request.POST)
+    #
+    #     if form.is_valid():
+    #         return self.form_valid_for_full(form, formset)
+    #     return self.form_invalid_for_full(form, formset)
+    #
+    #
+    # def form_valid_for_full(self, form, formset):
+    #     self.object = form.save(commit=True)
+    #     self.object.save()
+    #     formset.instance = self.object
+    #     formset.save()
+    #     # print(str(self.request.user) == 'expert_for_registation')
+    #     # if str(self.request.user) == 'expert_for_registation':
+    #     #     logout(self.request)
+    #     return HttpResponseRedirect(self.get_success_url())
+    #
+    # def form_invalid_for_full(self, form, formset):
+    #     return self.render_to_response(self.get_context_data(form=form, formset=formset))
+    #
+    # # def get_success_url(self):
+    # #     if str(self.request.user) == 'expert_for_registation':
+    # #         return reverse('webapp:expert_update', kwargs={'pk': self.object.pk})
+    # #     else:
+    # #         return reverse('webapp:expert_detail', kwargs={'pk': self.object.pk})
 
 
 class UserDetailView(DetailView):
