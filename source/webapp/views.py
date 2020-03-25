8d44398cb6ffd92f17bbd6b596f58280a517d222
akyl <akyl.aydarbekov@gmail.com>
2020-03-25 18:00:56 +0600

    добавил индекс вью

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from webapp.models import Ad
+
+
+class IndexView(ListView):
+    model = Ad
+    template_name = 'index.html'
+    context_object_name = 'ads'
